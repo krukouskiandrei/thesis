3f8c4dc50181c6ac661969070ce63a9dfc2e7322
Krukouski Andrei <andrei.krukouski@gmail.com>
2017-04-05 16:45:43 +0300

    developer: created MillerRabin panel

--- thesisApp/src/main/java/by/krukouski/thesisapp/frame/panel/MillerRabinAlgorithmPanel.java
+++ thesisApp/src/main/java/by/krukouski/thesisapp/frame/panel/MillerRabinAlgorithmPanel.java
@@ -1,6 +1,7 @@
 package by.krukouski.thesisapp.frame.panel;
 
 import java.awt.Font;
+import java.awt.Toolkit;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -10,27 +11,56 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextPane;
 import javax.swing.SpringLayout;
 import javax.swing.SwingConstants;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
 
+import by.krukouski.prime.algorithms.facades.MillerRabinFacades;
+import by.krukouski.prime.algorithms.facades.SolovayStrassenFacades;
+import by.krukouski.prime.algorithms.probability.MillerRabin;
 import by.krukouski.thesisapp.frame.panel.text.TextEditorPanel;
+import javax.swing.AbstractAction;
+import javax.swing.AbstractButton;
+
+import java.awt.event.ActionEvent;
+import java.math.BigInteger;
+import java.util.Enumeration;
+
+import javax.swing.Action;
 
 public class MillerRabinAlgorithmPanel extends JPanel {
 	
 	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private final Action action = new SwingAction();
+	private final TextEditorPanel textEditorPanel = new TextEditorPanel();
+	private final JLabel lblResult;
+	private final JLabel lblProbability;
+	private final JTextPane textPane;
 	public MillerRabinAlgorithmPanel() {
 		SpringLayout springLayout = new SpringLayout();
 		setLayout(springLayout);
 		
-		JTextPane textPane = new JTextPane();
+		textPane = new JTextPane(new DefaultStyledDocument() {
+		    @Override
+		    public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+		        if ((getLength() + str.length()) <= 5) {
+		            super.insertString(offs, str, a);
+		        }
+		        else {
+		            Toolkit.getDefaultToolkit().beep();
+		        }
+		    }
+		});
 		springLayout.putConstraint(SpringLayout.NORTH, textPane, 86, SpringLayout.NORTH, this);
 		springLayout.putConstraint(SpringLayout.WEST, textPane, -129, SpringLayout.EAST, this);
 		springLayout.putConstraint(SpringLayout.EAST, textPane, -51, SpringLayout.EAST, this);
 		add(textPane);
 		
-		JLabel lblProbability = new JLabel("Probability");
+		lblProbability = new JLabel("Probability");
 		springLayout.putConstraint(SpringLayout.EAST, lblProbability, -10, SpringLayout.WEST, textPane);
 		add(lblProbability);
 		
-		JLabel lblResult = new JLabel("Result");
+		lblResult = new JLabel("");
 		springLayout.putConstraint(SpringLayout.NORTH, lblResult, 72, SpringLayout.SOUTH, textPane);
 		lblResult.setHorizontalAlignment(SwingConstants.CENTER);
 		lblResult.setFont(new Font("Times New Roman", Font.BOLD, 19));
@@ -39,6 +69,7 @@ public class MillerRabinAlgorithmPanel extends JPanel {
 		add(lblResult);
 		
 		JButton btnCheck = new JButton("Check");
+		btnCheck.setAction(action);
 		springLayout.putConstraint(SpringLayout.SOUTH, lblResult, -40, SpringLayout.NORTH, btnCheck);
 		springLayout.putConstraint(SpringLayout.WEST, btnCheck, 188, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.SOUTH, btnCheck, -26, SpringLayout.SOUTH, this);
@@ -46,14 +77,66 @@ public class MillerRabinAlgorithmPanel extends JPanel {
 		add(btnCheck);
 		
 		//JPanel panel = new JPanel();
-		TextEditorPanel panel = new TextEditorPanel();
-		springLayout.putConstraint(SpringLayout.NORTH, panel, 10, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, panel, -12, SpringLayout.NORTH, textPane);
-		springLayout.putConstraint(SpringLayout.NORTH, lblProbability, 16, SpringLayout.SOUTH, panel);
-		springLayout.putConstraint(SpringLayout.WEST, panel, 101, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.EAST, panel, 0, SpringLayout.EAST, lblResult);
-		add(panel);
+		
+		springLayout.putConstraint(SpringLayout.NORTH, textEditorPanel, 10, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, textEditorPanel, -12, SpringLayout.NORTH, textPane);
+		springLayout.putConstraint(SpringLayout.NORTH, lblProbability, 16, SpringLayout.SOUTH, textEditorPanel);
+		springLayout.putConstraint(SpringLayout.WEST, textEditorPanel, 101, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.EAST, textEditorPanel, 0, SpringLayout.EAST, lblResult);
+		add(textEditorPanel);
 		
 	}
 
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "Check");
+			putValue(SHORT_DESCRIPTION, "Check number");
+		}
+		public void actionPerformed(ActionEvent e) {
+		
+			if(getValue(NAME).equals("Check")){
+				
+				try{
+					BigInteger number = getNumber();
+					Integer probability = getProbability();
+					checkNumber(number, probability);
+					
+				}catch(Exception exc){
+					//throw Exception!!!!
+					System.out.println("93");
+				}
+				
+				putValue(NAME, "Clean");
+				putValue(SHORT_DESCRIPTION, "Clean text editor");
+			}else{
+				lblResult.setText("");
+				textEditorPanel.cleanTextArea();
+				textPane.setText("");
+				putValue(NAME, "Check");
+				putValue(SHORT_DESCRIPTION, "Check number");
+			}
+			
+		}
+	}
+	
+	
+	
+	private BigInteger getNumber() throws NumberFormatException {
+		BigInteger number = new BigInteger(textEditorPanel.getText());
+		return number;
+	}
+	
+	private Integer getProbability() throws NumberFormatException {
+		Integer number = new Integer(textPane.getText());
+		return number;
+	}
+	
+	private void checkNumber(BigInteger number, Integer probability) {
+		boolean result = MillerRabinFacades.checkNumberForTest(number, probability);
+		if(result){
+			lblResult.setText("IS PRIME");
+		}else{
+			lblResult.setText("NOT PRIME");
+		}
+	}
 }
